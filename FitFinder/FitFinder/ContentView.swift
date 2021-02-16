821b6225c7110945a6c40ed063d2fa96c436bca8
Noah Frew <noahfrew@gmail.com>
2021-02-15 18:19:35 -0600

    Created Project with SwiftUI

---
+++ FitFinder/FitFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitFinder
+//
+//  Created by Noah Frew on 2/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
